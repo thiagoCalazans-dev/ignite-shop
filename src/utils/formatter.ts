9e2dcd528d84eb38f14942c94f756224c8afdd27
Thiago Calazans <devt.calazans@gmail.com>
2022-10-03 14:25:58 -0300

    modulo 2 completo - imagem error

---
+++ src/utils/formatter.ts
@@ -0,0 +1,8 @@
+
+export const priceFormatter = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  
+export const dateFormatter = new Intl.DateTimeFormat("pt-BR");
